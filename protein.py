2a1e5b45002bb6a269d8d6669963a11100645b3b
PhatchariyaTonlew <tonlew2110@gmail.com>
2024-10-03 15:11:19 +0700

    add Activitylevel

--- protein.py
+++ protein.py
@@ -5,5 +5,8 @@ def protein():
     st.write("You selected",SEX)
     weight = st.number_input("Weight")
     x=weight*0.8
+    Activitylevel = st.radio("Activity level",
+     ('Little/no exercise', 'Exercise often'))
+    st.write("You selected",Activitylevel)
     
 protein()
