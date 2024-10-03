b9a61207ba2faafaaa11b7a46a8ec797c3184d72
PhatchariyaTonlew <tonlew2110@gmail.com>
2024-10-03 14:51:10 +0700

    need bugfix

--- protein.py
+++ protein.py
@@ -1,11 +1,11 @@
 import  streamlit as st
 
 def Protein():
-    SEX = st.radio(
-     "SEX",
-     ('Female', 'Male'))
+    SEX = st.radio("What's your SEX",
+     ['Female', 'Male'],)
 
-    st.write(f"You selected {SEX}")
-    weight = st.number_input("Weight")
-    x=weight*0.8
+    st.write("You selected",SEX)
+    #weight = st.number_input("Weight")
+    #x=weight*0.8
+
 
