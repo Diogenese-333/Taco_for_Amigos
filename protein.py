1b53f506939197b7db591e380d45d0dc27d77de6
PhatchariyaTonlew <tonlew2110@gmail.com>
2024-10-10 15:51:32 +0700

    Pritein

--- protein.py
+++ protein.py
@@ -6,21 +6,23 @@ def protein():
     st.write("You selected:", SEX)
     #Height = st.number_input ("Height (in CM )",min_value=0)
     protein_needs= None
-    weight = st.number_input("Weight (in kg)", min_value=1,max_value=400,value=None)
+
     
     if SEX == 'Female':
+          weight = st.number_input("Weight (in kg)", min_value=1,max_value=400,value=None)
           if weight is not None:
              Activitylevel = st.radio("Activity level", 
              ('Select your activity level', 'Little/no exercise', 'Exercise often'))
              if Activitylevel == 'Little/no exercise':
-               protein_needs = weight * 0.8  # Example value for sedentary females
+                protein_needs = weight * 0.8  # Example value for sedentary females
              
              elif Activitylevel =='Exercise often':
                   protein_needs = weight * 1.0  # Example value for active females
              if    protein_needs  is not None:
-               if  Activitylevel != 'Little/no exercise''Exercise often':
-                 st.write('You need approximately', round(protein_needs, 2), 'grams of protein per day.')
+                 if  Activitylevel != 'Little/no exercise''Exercise often':
+                   st.write('You need approximately', round(protein_needs, 2), 'grams of protein per day.')
     if SEX == 'Male': 
+        weight = st.number_input("Weight (in kg)", min_value=1,max_value=400,value=None)
         if weight is not None:
              Activitylevel = st.radio("Activity level", 
              ('Select your activity level', 'Little/no exercise', 'Exercise often'))
